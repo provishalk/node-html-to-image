51bd3d248af09259ddd48ab248a6386308a6dca7
provishalk <provishalk@gmail.com>
2023-04-25 19:12:39 +0530

    first commit

---
+++ src/pages/api/generate-image.js
@@ -0,0 +1,20 @@
+import webshot from "node-webshot";
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    // handle POST request
+    webshot(
+      `<div>${req.body.text}</div>`,
+      "public/hello_world.png",
+      { siteType: "html", quality: 100 },
+      function (err) {
+        // screenshot now saved to hello_world.png
+        console.log(err);
+      }
+    );
+    res.status(200).json({ message: "POST request received" });
+  } else {
+    // handle other request methods
+    res.status(400).json({ message: "Invalid request method" });
+  }
+}
