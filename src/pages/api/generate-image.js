bba207fcbc1d4a17594232dfdd7f0baa64dbb2f7
provishalk <provishalk@gmail.com>
2023-04-25 19:19:24 +0530

    added logic back

--- src/pages/api/generate-image.js
+++ src/pages/api/generate-image.js
@@ -3,15 +3,15 @@ import webshot from "node-webshot";
 export default function handler(req, res) {
   if (req.method === "POST") {
     // handle POST request
-    // webshot(
-    //   `<div>${req.body.text}</div>`,
-    //   "public/hello_world.png",
-    //   { siteType: "html", quality: 100 },
-    //   function (err) {
-    //     // screenshot now saved to hello_world.png
-    //     console.log(err);
-    //   }
-    // );
+    webshot(
+      `<div>${req.body.text}</div>`,
+      "public/hello_world.png",
+      { siteType: "html", quality: 100 },
+      function (err) {
+        // screenshot now saved to hello_world.png
+        console.log(err);
+      }
+    );
     res.status(200).json({ message: "POST request received" });
   } else {
     // handle other request methods
