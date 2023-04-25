016e65b14497af3db4f3aadcab3b02181c79f527
provishalk <provishalk@gmail.com>
2023-04-25 19:23:22 +0530

    try catch added

--- src/pages/api/generate-image.js
+++ src/pages/api/generate-image.js
@@ -1,20 +1,20 @@
-import webshot from "node-webshot";
+import webshot from 'node-webshot';
 
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    // handle POST request
-    webshot(
-      `<div>${req.body.text}</div>`,
-      "public/hello_world.png",
-      { siteType: "html", quality: 100 },
-      function (err) {
-        // screenshot now saved to hello_world.png
-        console.log(err);
-      }
-    );
-    res.status(200).json({ message: "POST request received" });
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      await webshot(
+        `<div>${req.body.text}</div>`,
+        'public/hello_world.png',
+        { siteType: 'html', quality: 100 }
+      );
+      console.log('Screenshot saved successfully');
+      res.status(200).json({ message: 'Screenshot saved successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error saving screenshot' });
+    }
   } else {
-    // handle other request methods
-    res.status(400).json({ message: "Invalid request method" });
+    res.status(400).json({ message: 'Invalid request method' });
   }
 }
